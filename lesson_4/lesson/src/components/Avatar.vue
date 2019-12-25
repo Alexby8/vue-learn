419003d0a25b130a38763aa505dd0142fc6400da
Alexey <alexby8@yandex.ru>
2019-12-25 13:56:34 +0300

    Fix

--- lesson_4/lesson/src/components/Avatar.vue
+++ lesson_4/lesson/src/components/Avatar.vue
@@ -4,39 +4,20 @@
     <button type="button" class="btn btn-primary m-t-10 m-b-10" @click="selectFile">
       Select file
     </button>
-    <VueDropzone
-      id="dropzone"
-      ref="myVueDropzone"
-      :options="dropzoneOptions"
-      @vdropzone-complete="afterDropComplete"
-    ></VueDropzone>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import vue2Dropzone from 'vue2-dropzone'
-import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 
 export default {
   name: 'AvatarUploader',
-  components: {
-    VueDropzone: vue2Dropzone
-  },
   props: {
     value: {
       type: String,
       required: true
     }
   },
-  data: () => ({
-    dropzoneOptions: {
-      url: 'https://api.imgur.com/3/image',
-      thumbnailWidth: 150,
-      maxFilesize: 0.5,
-      headers: { Authorizaton: 'Client-ID: b39f08aeaec36d1' }
-    }
-  }),
   methods: {
     setNewAvatar(url) {
       this.$emit('input', url)
@@ -62,10 +43,6 @@ export default {
           this.setNewAvatar(response.data.link)
           this.$refs.file.value = ''
         })
-    },
-    afterDropComplete(result) {
-      console.log(result)
-      //this.setNewAvatar(response.data.link)
     }
   }
 }
